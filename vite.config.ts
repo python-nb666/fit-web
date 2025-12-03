24c3aa297ec9ab39f8f79a89455e965b30a46b74
weixiaoxi <xiaoxi.wei@transsion.com>
2025-12-03 11:02:07 +0800

    fix:build base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fit-web",
+  base: "/fit-web/",
   plugins: [react(), tailwindcss()],
 });
